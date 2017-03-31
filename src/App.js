b91ac3995ee55377287708d53cd6d48a902af106
alex cai <caiyingyao@ibbd.net>
2017-03-31 11:37:31 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import { ApolloProvider } from 'react-apollo';
 import TaskList from './TaskList';
 
 class App extends Component {
-  constructor(...args) {
-    super(...args);
+    constructor(...args) {
+        super(...args);
 
-    const networkInterface = createNetworkInterface('http://localhost:8000/graphql-json');
-    this.client = new ApolloClient({
-      networkInterface,
-      dataIdFromObject: r => r.id,
-    });
-  }
-  render() {
-    return (
-      <ApolloProvider client={this.client}>
-        <TaskList />
-      </ApolloProvider>
-    );
-  }
+        const networkInterface = createNetworkInterface('http://localhost:8088/graphql-json');
+        this.client = new ApolloClient({
+            networkInterface,
+            dataIdFromObject: r => r.id,
+        });
+    }
+    render() {
+        return (
+            <ApolloProvider client={this.client}>
+                <TaskList />
+            </ApolloProvider>
+        );
+    }
 }
 
 export default App;
